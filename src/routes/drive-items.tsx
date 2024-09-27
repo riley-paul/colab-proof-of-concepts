c5f27a82600a6b219b107eac8e8441fbdfd5bd72
Riley Paul <rileypaul@colabsoftware.com>
2024-09-27 08:15:49 -0600

    switch to Astro site

--- src/routes/drive-items.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import DriveItems from "@/components/drive-items";
-
-export const Route = createFileRoute("/drive-items")({
-  component: DriveItems,
-});
