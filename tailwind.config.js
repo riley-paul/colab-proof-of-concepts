16bf76ae3888deab4f0238d27fa74e6d86195015
Riley Paul <rileypaul@colabsoftware.com>
2024-11-13 10:46:57 -0700

    add dual truncation experiment

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,6 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
-      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
