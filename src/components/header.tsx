f695280bcd4734d9eb8e3cb432df7ecea1f78081
Riley Paul <rileypaul@colabsoftware.com>
2025-01-06 07:42:20 -0700

    clean up layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Button, TabNav } from "@radix-ui/themes";
 import React from "react";
-import RadixProvider from "./radix-provider";
-import { buttonVariants } from "./ui/button";
 
 const links = [
   {
@@ -19,27 +17,26 @@ const links = [
 
 const Header: React.FC<{ pathname: string }> = ({ pathname }) => {
   return (
-    <RadixProvider appearance="dark" style={{ minHeight: "auto" }}>
-      <header className="flex w-full items-center justify-between px-4">
-        <TabNav.Root>
-          {links.map((link) => (
-            <TabNav.Link href={link.href} active={link.href === pathname}>
-              {link.text}
-            </TabNav.Link>
-          ))}
-        </TabNav.Root>
-        <Button asChild className="font-mono" size="1" variant="ghost">
-          <a
-            href="https://github.com/rjp301/colab-proof-of-concepts"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <i className="fa-brands fa-github"></i>
-            <span>GitHub</span>
-          </a>
-        </Button>
-      </header>
-    </RadixProvider>
+    <header className="flex w-full items-center justify-between px-4 border-b">
+      <TabNav.Root>
+        {links.map((link) => (
+          <TabNav.Link href={link.href} active={link.href === pathname}>
+            <i className={cn("mr-3", link.icon)} />
+            {link.text}
+          </TabNav.Link>
+        ))}
+      </TabNav.Root>
+      <Button asChild className="font-mono" size="1" variant="ghost">
+        <a
+          href="https://github.com/rjp301/colab-proof-of-concepts"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i className="fa-brands fa-github"></i>
+          <span>GitHub</span>
+        </a>
+      </Button>
+    </header>
   );
 };
 
--- src/features/drive/index.tsx
+++ src/features/drive/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import DrivePragmaticItems from "./components/drive-pragmatic/drive-pragmatic-items";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
 import { cn } from "@/lib/utils";
 import { useAtom } from "jotai";
 import { isMultiSelectModeAtom, selectionAtom } from "./store";
 import useScrollShadow from "@/hooks/use-scroll-shadow";
 import useSelection from "@/hooks/use-selection";
-import { IconButton, Tooltip } from "@radix-ui/themes";
+import { Card, Heading, IconButton, Text, Tooltip } from "@radix-ui/themes";
 import RadixProvider from "@/components/radix-provider";
 
 const DriveDemo: React.FC = () => {
@@ -23,19 +17,19 @@ const DriveDemo: React.FC = () => {
   const { selection, selectLast } = useSelection(selectionAtom);
 
   return (
-    <RadixProvider>
-      <Card>
-        <CardHeader
+    <RadixProvider style={{ minHeight: "auto" }}>
+      <Card size="3">
+        <header
           className={cn(
             "transitional-colors border-b border-transparent ease-in-out",
             isScrolled && "border-border",
           )}
         >
           <div className="flex items-center justify-between">
-            <CardTitle className="text-2xl font-bold">
+            <Heading className="text-2xl font-bold">
               <i className="fa-solid fa-hdd mr-3" />
               <span>Drive</span>
-            </CardTitle>
+            </Heading>
             <div className="flex items-center gap-3">
               <span className="text-xs text-muted-foreground">
                 {selection.size} items selected
@@ -53,18 +47,15 @@ const DriveDemo: React.FC = () => {
               </Tooltip>
             </div>
           </div>
-          <CardDescription>
+          <Text>
             Mockup of Drive using Pragmatic Drag and Drop library. Try dragging
             an item onto another item to change it's parent, or move multiple
             items at a time using multi-select mode.
-          </CardDescription>
-        </CardHeader>
-        <CardContent
-          ref={listRef}
-          className="grid max-h-[30rem] overflow-auto px-3"
-        >
+          </Text>
+        </header>
+        <div ref={listRef} className="grid max-h-[30rem] overflow-auto px-3">
           <DrivePragmaticItems />
-        </CardContent>
+        </div>
       </Card>
     </RadixProvider>
   );
